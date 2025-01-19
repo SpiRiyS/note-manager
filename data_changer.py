a65662d2d25e028ba10eec9c6193823c6e339653
SpiRiyS <zommerfelt@mail.ru>
2025-01-19 10:16:53 +0300

    grade1 задание 2
    преоброзование строк

---
+++ data_changer.py
@@ -0,0 +1,15 @@
+username='Kirill'
+title='заголовок'
+content='описание'
+status='начало'
+created_data='19.07.2025'
+issue_date='19.01.2026'
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_data)
+print(issue_date)
+date_1=input('введите дату')
+print(date_1)
+print(date_1[:-5])
